752083b262dbd9c9c742264328df0e8bac698b1f
nbruski <nikolas.bruski@interia.eu>
2024-06-25 12:47:53 +0200

    added docker-compose, changed packaging and changed application.properties to .yaml
    Signed-off-by: nbruski <nikolas.bruski@interia.eu>

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,9 +26,11 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.liquibase:liquibase-core")
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.h2database:h2")
     annotationProcessor("org.projectlombok:lombok")
+    annotationProcessor ("org.mapstruct:mapstruct-processor:1.5.5.Final")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
