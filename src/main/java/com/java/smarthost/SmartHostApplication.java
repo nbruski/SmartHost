ec16a70cd181822b6eb8b431389ed7d6deaec4cc
nbruski <nikolas.bruski@interia.eu>
2024-06-25 12:04:56 +0200

    initial commit
    Signed-off-by: nbruski <nikolas.bruski@interia.eu>

---
+++ src/main/java/com/java/smarthost/SmartHostApplication.java
@@ -0,0 +1,13 @@
+package com.java.smarthost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartHostApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/smarthost/SmartHostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.smarthost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
